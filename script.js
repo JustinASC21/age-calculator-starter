d2bd89902520e2c2866467b6bc1fbcb03da18dde
Glitch (lava-diagnostic-arch) <none>
2021-11-09 23:27:10 +0000

    📕🐡 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+let birthYear = 0;
+let currentYear;
+let age;
+let name = "";
 
+// calculate age button clicked
+$("button").click(function() {
+  birthYear = $(".year").val();
+  name = $(".name").val();
+  currentYear = $(".currentYear").val();
+  age = currentYear - birthYear;
+  $(".message").text(name + " you look to be " + age + " years old by " + currentYear);
+  
+  // clear fields
+  $(".currentYear").val("");
+  $(".name").val("");
+  $(".year").val("");
+})
